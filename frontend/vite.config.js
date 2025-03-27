ed4bc1b4a22e2642ea7fa08072f2453ef2921ac0
phat0034 <cf.log.bro36@gmail.com>
2025-03-27 21:25:34 +0700

    fix sthing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Ecommerce-store-with-Reactj-Laravel-/', // 👈 Thêm dòng này
+  base: '/ecommerce-store-with-reactj-laravel-', // 👈 Thêm dòng này
 })
